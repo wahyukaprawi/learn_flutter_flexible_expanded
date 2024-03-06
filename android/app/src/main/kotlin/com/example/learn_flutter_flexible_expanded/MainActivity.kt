d4ff9208cd4bfd8d7c7b6e8b21692b07a32180cd
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-03-06 18:27:25 +0800

    add code

---
+++ android/app/src/main/kotlin/com/example/learn_flutter_flexible_expanded/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learn_flutter_flexible_expanded
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Learn Flexible and Expanded'),
+      ),
+      body: SizedBox(
+        child: Column(
+          children: [
+            Flexible(
+              flex: 3,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.red,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.amber,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.green,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.blue,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 10,
+              child: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Colors.red,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.amber,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.green,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.blue,
+                      ),
+                    )
+                  ],
+                ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
